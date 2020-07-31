401c2553cd9106c431e9df2404d2626a7a1d2382
Ballem <romarcio@gmail.com>
2020-07-31 17:51:53 -0300

    comitando pacote usuario

---
+++ caracteristicas-da-linguagem/src/main/java/one/digitalinnovation/classes/usuario/SuperUsuario.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.classes.usuario;
+
+public class SuperUsuario {
+
+    private String login;
+    private String senha;
+    String nome;
+
+    public SuperUsuario(final String login, final String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
