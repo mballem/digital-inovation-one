54394dafbed7a703035fbfd8df011fc30f7a5e06
Ballem <romarcio@gmail.com>
2020-08-01 13:32:09 -0300

    Java Wrappers

---
+++ caracteristicas-da-linguagem/src/main/java/one/digitalinnovation/classes/tipos/wrappers/Wrappers.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.classes.tipos.wrappers;
+
+public class Wrappers {
+
+    public static void main(String[] args) {
+        // UNBOXING
+        int i1 = new Integer("5");
+        int i2 = Integer.valueOf("5");
+
+        // AUTOBOXING
+        Integer i3 = 5;
+    }
+}
