23dfb27d2a1df638ade87a9883574990843a485a
Ballem <romarcio@gmail.com>
2020-08-06 14:54:49 -0300

    primeiro commit Spring Boot Hello

---
+++ spring-boot-one-innovation/src/test/java/one/innovation/springboot/SpringBootOneInnovationApplicationTests.java
@@ -0,0 +1,13 @@
+package one.innovation.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOneInnovationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
