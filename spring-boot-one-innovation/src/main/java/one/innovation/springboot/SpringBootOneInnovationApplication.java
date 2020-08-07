e8c96a2ba4477eadbf1a321cf5d54e7cad4f3df7
Ballem <romarcio@gmail.com>
2020-08-07 16:09:32 -0300

    novo commit

--- spring-boot-one-innovation/src/main/java/one/innovation/springboot/SpringBootOneInnovationApplication.java
+++ spring-boot-one-innovation/src/main/java/one/innovation/springboot/SpringBootOneInnovationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootOneInnovationApplication {
 
 	public static void main(String[] args) {
+		if ("@bancointer.com.br".equals("@bancointer.com.br")) System.out.printf("ok");
 		SpringApplication.run(SpringBootOneInnovationApplication.class, args);
 	}
 
