e3ec504e1cc28a5a4bda4c9deedfe0b0542c09eb
Ballem <romarcio@gmail.com>
2020-08-07 16:10:07 -0300

    atualizacao classe principal

--- spring-boot-one-innovation/src/main/java/one/innovation/springboot/SpringBootOneInnovationApplication.java
+++ spring-boot-one-innovation/src/main/java/one/innovation/springboot/SpringBootOneInnovationApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootOneInnovationApplication {
 
 	public static void main(String[] args) {
-		if ("@bancointer.com.br".equals("@bancointer.com.br")) System.out.printf("ok");
 		SpringApplication.run(SpringBootOneInnovationApplication.class, args);
 	}
 
