dcf219f5279ecfe4defcf8669f56ddce264ec6e7
Ballem <romarcio@gmail.com>
2020-08-10 15:20:55 -0300

    pacote da classe renomeado de springbootconfig para springbootprofiles

--- spring-boot-profiles/src/main/java/one/innovation/springbootprofiles/SpringBootConfigApplication.java
+++ spring-boot-profiles/src/main/java/one/innovation/springbootprofiles/SpringBootConfigApplication.java
@@ -1,4 +1,4 @@
-package one.innovation.springbootconfig;
+package one.innovation.springbootprofiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
