d8b85c4f8825c889e2d0abc07e4eebf9f0a158eb
Ballem <romarcio@gmail.com>
2020-08-10 16:23:04 -0300

    lendo variavies de ambiente com @Value

--- spring-boot-profiles/src/main/java/one/innovation/springbootprofiles/AppController.java
+++ spring-boot-profiles/src/main/java/one/innovation/springbootprofiles/AppController.java
@@ -10,8 +10,24 @@ public class AppController {
     @Value("${app.message}")
     private String appMessage;
 
+    @Value("${ENV_DB_URL:NOT_FOUND}")
+    private String dbEnvironmentVariable;
+
+    @Value("${JAVA_HOME:NOT_FOUND}")
+    private String javaHomeEnv;
+
     @GetMapping("/")
     public String getAppMessage() {
         return appMessage;
+    }
+
+    @GetMapping("/envVariable")
+    public String getDbEnvironmentVariable() {
+        return "A seguinte variavel de ambiente foi passada: " + dbEnvironmentVariable;
+    }
+
+    @GetMapping("/javaHome")
+    public String getJavaHome() {
+        return "O valor de Java Home é: " + javaHomeEnv;
     }
 }
