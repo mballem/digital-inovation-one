38cd5998b185e5eedce7805027709311679a4e13
Ballem <romarcio@gmail.com>
2020-08-10 15:29:55 -0300

    adicionada classe de configuração database

---
+++ spring-boot-profiles/src/main/java/one/innovation/springbootprofiles/config/DBConfiguration.java
@@ -0,0 +1,67 @@
+package one.innovation.springbootprofiles.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties("spring.datasource")
+public class DBConfiguration {
+
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    @Profile("dev")
+    @Bean
+    public String testDatabaseConnection() {
+        System.out.println("DB connection for DEV - H2");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB connection to H2_TEST - Test instance";
+    }
+
+    @Profile("prod")
+    @Bean
+    public String productionDatabaseConnection() {
+        System.out.println("DB connection for PROD - MySQL");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB connection to MYSQL_PROD - Production instance";
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
