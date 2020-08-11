b6a87280aae6b24107677fc7e99c55bcc2f1a4ff
Ballem <romarcio@gmail.com>
2020-08-11 17:31:35 -0300

    Imutabilidade com UnaryOperator

---
+++ functional-java/src/main/java/one/innovation/functional/Imutabilidade.java
@@ -0,0 +1,12 @@
+package one.innovation.functional;
+
+import java.util.function.UnaryOperator;
+
+public class Imutabilidade {
+    public static void main(String[] args) {
+        int valor = 20;
+        UnaryOperator<Integer> retornoDobro = v -> v * 2;
+        System.out.println(retornoDobro.apply(valor));
+        System.out.println(valor);
+    }
+}
