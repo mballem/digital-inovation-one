61c6425f1c28111ce4f395f2ae0b3532abc589a6
Ballem <romarcio@gmail.com>
2020-08-11 17:23:16 -0300

    Primeiro commit

---
+++ functional-java/src/main/java/one/innovation/functional/ComposicaoDeFuncoes.java
@@ -0,0 +1,26 @@
+package one.innovation.functional;
+
+import java.util.Arrays;
+
+public class ComposicaoDeFuncoes {
+    public static void main(String[] args) {
+        int[] valores = {1, 2, 3, 4};
+
+        // Funcional
+        Arrays.stream(valores)
+                .filter(n -> n % 2 == 0) // se n for par
+                .map(n -> n * 2) // multiplica n por 2
+                .forEach(n -> System.out.println("Functional: " + n)); // imprime n quando par
+
+        // Imperative
+        for (int i = 0; i < valores.length; i++) {
+            int n = 0;
+            if (valores[i] % 2 == 0) {
+                n = valores[i] * 2;
+                if (n != 0) {
+                    System.out.println("Imperative: " + n);
+                }
+            }
+        }
+    }
+}
