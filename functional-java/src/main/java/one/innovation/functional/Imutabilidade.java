6e7fcd369b25f569144ee039c41c502ccf02fe07
Ballem <romarcio@gmail.com>
2020-08-11 17:32:56 -0300

    add comentários

--- functional-java/src/main/java/one/innovation/functional/Imutabilidade.java
+++ functional-java/src/main/java/one/innovation/functional/Imutabilidade.java
@@ -6,7 +6,7 @@ public class Imutabilidade {
     public static void main(String[] args) {
         int valor = 20;
         UnaryOperator<Integer> retornoDobro = v -> v * 2;
-        System.out.println(retornoDobro.apply(valor));
-        System.out.println(valor);
+        System.out.println(retornoDobro.apply(valor)); // retorna o dobro
+        System.out.println(valor); // retorna o valor original
     }
 }
