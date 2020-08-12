b00eae1919e6db96debbb50f5f330582af22f4db
Ballem <romarcio@gmail.com>
2020-08-12 16:14:32 -0300

    add novas operações

--- functional-java/src/main/java/one/innovation/functional/aula2/FuncaoAltaOrdem.java
+++ functional-java/src/main/java/one/innovation/functional/aula2/FuncaoAltaOrdem.java
@@ -12,13 +12,22 @@ public class FuncaoAltaOrdem {
 
         Calculo somaComReferencia = Integer::sum;
         System.out.println(executarOperacao(somaComReferencia, 1, 3));
+
+        Calculo subtracao = (a, b) -> a - b;
+        System.out.println(executarOperacao(subtracao, 4, 3));
+
+        Calculo multiplicacao = (a, b) -> a * b;
+        System.out.println(executarOperacao(multiplicacao, 1, 3));
+
+        Calculo divisao = (a, b) -> a / b;
+        System.out.println(executarOperacao(divisao, 14, 2));
     }
 
     public static int executarOperacao(Calculo calculo, int a, int b) {
-        return calculo.somar(a, b);
+        return calculo.calcular(a, b);
     }
 }
 
 interface Calculo {
-    int somar(int a, int b);
+    int calcular(int a, int b);
 }
