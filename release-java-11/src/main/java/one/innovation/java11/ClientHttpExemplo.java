a82c22d461101f4b70faae74338312522830044c
Ballem <romarcio@gmail.com>
2020-08-17 15:02:22 -0300

    HttpRequest, HttpClient e HttpResponse

---
+++ release-java-11/src/main/java/one/innovation/java11/ClientHttpExemplo.java
@@ -0,0 +1,32 @@
+package one.innovation.java11;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ClientHttpExemplo {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        connectAndPrintUrlContent();
+    }
+
+    private static void connectAndPrintUrlContent() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET().uri(URI.create("https://openjdk.java.net/"))
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Status Code: " + response.statusCode());
+
+        response.headers().map().forEach((k,v) -> System.out.println(k+":"+v));
+
+        System.out.println(response.body());
+    }
+
+}
