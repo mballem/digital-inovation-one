0d2e2db7adb44099c94e50a9b2e11c2f94092e5c
Ballem <romarcio@gmail.com>
2020-08-17 16:16:22 -0300

    Inicializando lista com List.of

--- release-java-11/src/main/java/one/innovation/java11/CollectionExemplo.java
+++ release-java-11/src/main/java/one/innovation/java11/CollectionExemplo.java
@@ -1,17 +1,18 @@
 package one.innovation.java11;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class CollectionExemplo {
 
     public static void main(String[] args) {
-        List<String> nomes = Arrays.asList("João", "Bia", "Tito", "Nunes", "Carla", "Iago");
+        List<String> nomes = Arrays.asList("João", "Beatriz", "Antônio", "Nunes", "Carla", "Iago");
         System.out.println(nomes);
+
+        List<String> apelidos = List.of("Joca", "Bia", "Tonho", "Nu", "Carlinha", "Igo");
+        System.out.println(apelidos);
 
         Collection<String> marcas = Set.of("Ford", "GM", "Honda", "Ferrari", "Mac Laren", "Fiat", "Yamaha");
         System.out.println(marcas);
+
     }
 }
