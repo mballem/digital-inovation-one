2f4a9c5438fa46b3c32cb8c99cb4c1ce86314cf0
Ballem <romarcio@gmail.com>
2020-08-18 15:38:03 -0300

    classe principal

---
+++ demo-rest-mvc/src/main/java/one/innovation/demorestmvc/DemoRestMvcApplication.java
@@ -0,0 +1,13 @@
+package one.innovation.demorestmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestMvcApplication.class, args);
+	}
+
+}
