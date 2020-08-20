7274344219dd2255f720545b252e94a24ed23ae8
Ballem <romarcio@gmail.com>
2020-08-20 14:42:53 -0300

    add classe de testes

---
+++ demo-restfull/src/test/java/one/innovation/demorestfull/DemoRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package one.innovation.demorestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
