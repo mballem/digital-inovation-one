1f21735971a30341f6a4ecb8797e113f02a07c80
Ballem <romarcio@gmail.com>
2020-08-27 15:22:34 -0300

    commit aula js

---
+++ aula_javascript/js/main.js
@@ -0,0 +1,27 @@
+var nome = "José Beltrano";
+var idade = 30;
+
+//alert("Bem vindo " + nome + ", você tem " + idade + " anos.");
+
+console.log("Bem vindo " + nome + ", você tem " + idade + " anos.");
+
+var sobrenome = "Fulano";
+console.log(nome.replace("Beltrano", "Fulano"));
+console.log(nome.toUpperCase());
+console.log(nome.toLowerCase());
+
+var n1 = 10;
+var n2 = 5;
+console.log(n1 * n2);
+
+var n3 = "10";
+var n4 = "5";
+console.log(n3 * n4);
+
+var s1 = 10;
+var s2 = 5;
+console.log(s2 + s2);
+
+var s3 = "10";
+var s4 = "5";
+console.log(s3 + s4);
