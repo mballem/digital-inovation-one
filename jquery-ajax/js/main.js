ae7816b1ca8d2e7f0a1817a695662b1cdd07d206
Ballem <romarcio@gmail.com>
2021-02-03 16:14:00 -0300

    busca cep

--- jquery-ajax/js/main.js
+++ jquery-ajax/js/main.js
@@ -1,1 +1,16 @@
-alert("Jquery");
+function consultaCep() {
+    var cep = document.getElementById("cep").value;
+    $.ajax({
+        url: 'https://viacep.com.br/ws/' + cep + '/json/',
+        type: 'GET',
+        success: function(response){
+            console.log(response);
+            $("#cep").html(response.cep); // com jquery
+            document.getElementById("logradouro").innerHTML = response.logradouro;
+            document.getElementById("complemento").innerHTML = response.complemento;
+            document.getElementById("bairro").innerHTML = response.bairro;
+            document.getElementById("localidade").innerHTML = response.localidade;
+            $("#uf").html(response.uf); // com jquery
+        }  
+    })
+}
