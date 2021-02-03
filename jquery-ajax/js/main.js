9379aa18ed9c50c4a26bd63562eb41bfdf2063dc
Ballem <romarcio@gmail.com>
2021-02-03 17:15:58 -0300

    barra de progresso, hide e show

--- jquery-ajax/js/main.js
+++ jquery-ajax/js/main.js
@@ -3,6 +3,9 @@ function consultaCep() {
     $.ajax({
         url: 'https://viacep.com.br/ws/' + cep + '/json/',
         type: 'GET',
+        beforeSend: function() {
+            $(".barra-progresso").show();
+        },
         success: function(response){
             console.log(response);
             //$("#logradouro").html(response.logradouro); // com jquery
@@ -13,7 +16,14 @@ function consultaCep() {
             $("#localidade").html(response.localidade);
             $("#uf").html(response.uf);
             $("#numero_cep").html('CEP: ' + response.cep);
-
+            $(".cep").show();   
+            $(".barra-progresso").hide(); 
         }  
     })
 }
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+})
+
