ace9875b57be51cb9ba6b3f15e87fddb4ba4cdca
Marcio Ballem <romarcio@gmail.com>
2021-08-25 14:37:07 -0300

    Estudo sobre atribuição por referencia

---
+++ capgemini/java/estrutura_de_dados/aula/src/main/java/org/example/atribreferencia/MeuObj.java
@@ -0,0 +1,22 @@
+package org.example.atribreferencia;
+
+public class MeuObj {
+    private Integer num;
+
+    public MeuObj(Integer num) {
+        this.num = num;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return this.num.toString();
+    }
+}
