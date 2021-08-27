1183b1a2742c74de290f2268489fa1da75813353
Marcio Ballem <romarcio@gmail.com>
2021-08-27 14:58:58 -0300

    adicionada exemplo de conexão JDBC

---
+++ capgemini/java/bancos/aula-jdbc/src/main/java/dio/ConnectionJDBC.java
@@ -0,0 +1,19 @@
+package dio;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionJDBC {
+
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/dio_jdbc?serverTimezone=America/Sao_Paulo";
+        Connection conn = null;
+
+        try (Connection connection = conn = DriverManager.getConnection(url, "root", "root")) {
+            System.out.println("SUCESSO");
+        } catch (SQLException e) {
+            System.out.println("FALHA: " + e.getMessage());
+        }
+    }
+}
