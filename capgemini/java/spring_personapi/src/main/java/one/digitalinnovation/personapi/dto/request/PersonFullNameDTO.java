8346c13be5c5bbdb36bf52d5a3eeb6cd4b48fac8
Ballem <romarcio@gmail.com>
2021-08-31 15:23:56 -0300

    Adicionado DTO PersonFullNameDTO para ser usado na operação de update parcial da entidade Person

---
+++ capgemini/java/spring_personapi/src/main/java/one/digitalinnovation/personapi/dto/request/PersonFullNameDTO.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.personapi.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonFullNameDTO {
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String firstName;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String lastName;
+}
