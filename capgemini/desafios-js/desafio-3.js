b9274c087ab2a6dc55a23219b6196b9b600281a6
Ballem <romarcio@gmail.com>
2021-09-23 15:51:59 -0300

    desafio 3 js - Nome no Formulario

---
+++ capgemini/desafios-js/desafio-3.js
@@ -0,0 +1,8 @@
+//Para teste
+//const texto = "Fulano de tal" 
+//+ " Pedro de Alcantara Francisco Antonio Joao Carlos Xavier de Paula Miguel Rafael Joaquim Jose Gonzaga Pascoal Cipriano Serafim";
+
+//implemente sua solução aqui
+//const texto = gets();
+const size = texto.length;
+console.log((size >= 1 && size <= 80) ? 'YES' : 'NO');
