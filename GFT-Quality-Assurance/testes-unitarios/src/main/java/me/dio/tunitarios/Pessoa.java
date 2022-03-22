981dedc3e85bf435149061b28bfa70d615978714
Ballem <romarcio@gmail.com>
2022-03-22 16:52:13 -0300

    adicionados testes de unidade para hobbies

--- GFT-Quality-Assurance/testes-unitarios/src/main/java/me/dio/tunitarios/Pessoa.java
+++ GFT-Quality-Assurance/testes-unitarios/src/main/java/me/dio/tunitarios/Pessoa.java
@@ -2,6 +2,8 @@ package me.dio.tunitarios;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Pessoa {
 
@@ -9,9 +11,17 @@ public class Pessoa {
 
     private LocalDate nascimento;
 
+    private String[] hobbies;
+
     public Pessoa(String nome, LocalDate nascimento) {
         this.nome = nome;
         this.nascimento = nascimento;
+    }
+
+    public Pessoa(String nome, LocalDate nascimento, String... hobbies) {
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.hobbies = hobbies;
     }
 
     public int getIdade() {
@@ -20,5 +30,17 @@ public class Pessoa {
 
     public boolean isMaiorIdade() {
         return getIdade() >= 18;
+    }
+
+    public String[] getHobbies() {
+        return this.hobbies;
+    }
+
+    public int totalDeHobbies() {
+        return this.hobbies.length;
+    }
+
+    public String getHobby(String hobby) {
+        return Arrays.stream(this.hobbies).filter(h -> h.equals(hobby)).collect(Collectors.joining());
     }
 }
--- GFT-Quality-Assurance/testes-unitarios/src/test/java/me/dio/tunitarios/PessoaTest.java
+++ GFT-Quality-Assurance/testes-unitarios/src/test/java/me/dio/tunitarios/PessoaTest.java
@@ -8,6 +8,35 @@ import java.time.LocalDate;
 class PessoaTest {
 
     @Test
+    void validaSeHobbyExiste() {
+        Pessoa pessoa =  new Pessoa("Joey",
+                LocalDate.of(2000,1,1),
+                "Filmes", "Futebol", "Música");
+
+        Assertions.assertEquals("Futebol", pessoa.getHobby("Futebol"));
+
+        Assertions.assertNotEquals("Anime", pessoa.getHobby("Anime"));
+    }
+
+    @Test
+    void validaTotalDeHobbies() {
+        Pessoa pessoa =  new Pessoa("Joey",
+                LocalDate.of(2000,1,1),
+                "Filmes", "Futebol", "Música");
+
+        Assertions.assertEquals(3, pessoa.totalDeHobbies());
+    }
+
+    @Test
+    void validaHobbies() {
+        Pessoa pessoa =  new Pessoa("Joey",
+                LocalDate.of(2000,1,1),
+                "Filmes", "Futebol", "Música");
+
+        Assertions.assertArrayEquals(new String[] {"Filmes", "Futebol", "Música"}, pessoa.getHobbies());
+    }
+
+    @Test
     void validaIdadePessoa() {
         Pessoa pessoa = new Pessoa("Joey", LocalDate.of(2000, 1, 1));
         Assertions.assertEquals(22, pessoa.getIdade());
