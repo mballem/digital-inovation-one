b778ab467674939740f7264219e8fe06eef89050
Ballem <romarcio@gmail.com>
2023-01-02 16:05:50 -0300

    atualizacao dos componentes angular

--- cloud-fullstack-warburg-pincus/angular-blog/src/app/components/title/title.component.ts
+++ cloud-fullstack-warburg-pincus/angular-blog/src/app/components/title/title.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.css']
+  styleUrls: ['./title.component.css', './title.responsive.component.css']
 })
 export class TitleComponent implements OnInit {
 
