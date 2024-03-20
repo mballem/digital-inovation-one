64eb10c5683b3ffbc166025d68604a7d26b4f687
Ballem <romarcio@gmail.com>
2024-03-20 10:49:13 -0300

    entrega desafios 2

---
+++ formacao-logica-programacao/desafios-2/index.js
@@ -0,0 +1,9 @@
+// Entrada de dados. Lembre-se: O parseInt(()) é importante para a conversão dos valores de entrada(String) para um valor numérico(int).
+let num1 = parseInt("45");
+let num2 = parseInt("40");
+
+//TODO: Implemente a lógica para exibir o xpGanho:
+const xpGanho = num1 * num2 * 100;
+
+// Imprime a quantidade de XP ganho
+print("Voce ganhou " + xpGanho + " XP!");
