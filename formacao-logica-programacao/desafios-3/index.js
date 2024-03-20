1100317893bb3a79837853654721077e65685665
Ballem <romarcio@gmail.com>
2024-03-20 10:59:31 -0300

    entrega desafios 3

---
+++ formacao-logica-programacao/desafios-3/index.js
@@ -0,0 +1,19 @@
+// Entrada de dados. Lembre-se: O parseInt(()) é importante para a conversão dos valores de entrada(String) para um valor numérico(int).
+let escolhaDoTreinador = parseInt("2");
+let pokemonEscolhido; 
+
+//TODO: Implemente as condições necessárias para a solução do desafio. Utilize a tabela de exemplos para identificar a escolha do treinador:
+if ( escolhaDoTreinador === 1) {
+    pokemonEscolhido = "Bulbasaur";
+} else if ( escolhaDoTreinador === 2) {
+    pokemonEscolhido = "Charmander";
+} else if ( escolhaDoTreinador === 4) {
+    pokemonEscolhido = "Pikachu";
+} else if (escolhaDoTreinador === 5) {
+    pokemonEscolhido = "Mewtwo";
+}
+
+//Imprime o Pokémon escolhido:
+if (pokemonEscolhido) {
+    print("Voce escolheu o " + pokemonEscolhido + " como seu Pokemon inicial.");
+}
