a4f2c3efc4f3ee07f8970114067a3c7656f2fed4
Ballem <romarcio@gmail.com>
2024-04-02 17:13:08 -0300

    adicionado recurso para converter de Celsius para Fahrenheit

--- formacao-git-certifrication/api-temperaturas/index.js
+++ formacao-git-certifrication/api-temperaturas/index.js
@@ -9,6 +9,13 @@ app.get('/fahrenheit-to-celsius/:fahrenheit', (req, res) => {
     res.json({ fahrenheit, celsius });
 });
 
+// Recurso para converter de Celsius para Fahrenheit
+app.get('/celsius-to-fahrenheit/:celsius', (req, res) => {
+    const celsius = parseFloat(req.params.celsius);
+    const fahrenheit = (celsius * 9/5) + 32;
+    res.json({ celsius, fahrenheit });
+});
+
 app.listen(8080 , () => {
     let data = new Date();
     console.log('Servidor node iniciado em: ' + data);
